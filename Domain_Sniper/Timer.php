5476e5a1e24839aa899b57bbee967b43cdad444e
David Khacherian <dmkhacherian@csupomona.edu>
2011-12-03 11:53:51 -0800

    P3

--- Domain_Sniper/Timer.php
+++ Domain_Sniper/Timer.php
@@ -1,10 +1,12 @@
-<?php function add_job($user, $start_month, $start_day, $start_hour, $end_month, $end_day, $end_hour, $command){
-	$f_ptr = fopen("crontab.list", "a+") or die("File could not be opened/created.");
+<?php function add_job($crontab_list, $user, $start_month, $start_day, $start_hour, $end_month, $end_day, $end_hour, $command){
+	$f_ptr = fopen($crontab_list, "a+") or die("File could not be opened/created.");
 	fwrite($f_ptr, "* ".$start_hour."-".$end_hour." ".$start_day."-".$end_day." ".$start_month."-".$end_month." * ".$user." ".$command);
-	
+	fclose($f_ptr);
 	}
-	function remove_job($line_number){
-	
+	function remove_job($crontab_list, $line_number){
+	$fileArray = file($crontab_list);
+	unset($fileArray[$line_number]);
+	file_put_contents($crontab_list, implode('', $fileArray), LOCK_EX);
 	}
 	function copy_jobs(){
 	
